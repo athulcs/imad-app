9cd9960129617a4b8b5589d58f4df84c62cc2417
athulcs <athulcs.cs@gmail.com>
2017-08-15 15:22:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,6 +76,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/stewie.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'stewie.png'));
 });
