ec4af5f04c5fb8516b5ce379c49852a0360329a6
athulcs <athulcs.cs@gmail.com>
2017-08-15 15:57:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ marginLeft=marginLeft+5;
 img.style.marginLeft=marginLeft+'px';
 }
     img.onclick = function() {
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,50);
 };
