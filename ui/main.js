072bccf0416c0318b0becc689d01c4f213b08c39
athulcs <athulcs.cs@gmail.com>
2017-08-15 15:49:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded');
 var img=document.getElementById('stew');
 var marginLeft=0;
 function moveRight(){
-marginLeft=marginLeft+10;
+marginLeft=marginLeft+1;
 img.style.marginLeft=marginLeft+'px';
 }
     img.onclick = function() {
