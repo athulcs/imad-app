8ad58bbb016f5f0654c6d193d911251a4b823b44
athulcs <athulcs.cs@gmail.com>
2017-08-17 19:39:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,3 +8,9 @@ img.style.marginLeft=marginLeft+'px';
     img.onclick = function() {
     var interval = setInterval(moveRight,50);
 };
+var button =document.getElementById('counter');
+button.onclick = function() {
+    counter = counter +1;
+    var span =document.getElementById('count');
+    span.innerHTML=counter.toString();
+}
