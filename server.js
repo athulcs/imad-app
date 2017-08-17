e4ee5aad2a74af530da7bc48344120f710df823c
athulcs <athulcs.cs@gmail.com>
2017-08-17 19:24:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,6 +66,7 @@ var htmltemplate=
 ;
 return htmltemplate;
 }
+
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
@@ -89,6 +90,11 @@ app.get('/:articleName', function (req, res) {
    res.send(createTemplate(articles[articleName])) ;
 });
 
+var counter =0;
+app.get('/counter',function(req,res){
+counter =counter+1;
+res.send(counter,tostring());
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
