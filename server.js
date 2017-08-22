44d0ade078c4d78196af16540b1c85bbee543439
athulcs <athulcs.cs@gmail.com>
2017-08-22 22:52:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var counter=0;
 var Pool = require('pg').Pool;
 
 var config = {
-    user :	'athulcscs',
-    database : 'athulcscs',
-    host:'http://db.imad.hasura-app.io',
+    user :'athulcscs',
+    database :'athulcscs',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password: process.env.DB_PASSWORD
 };
